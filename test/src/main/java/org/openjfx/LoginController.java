c0ec611bcec87fb39eb5e45e9e834d545a4f73fd
Ealuthwala <e_aluthwala@hotmail.com>
2020-04-29 08:52:55 +0800

    Add files via upload

---
+++ test/src/main/java/org/openjfx/LoginController.java
@@ -0,0 +1,43 @@
+package org.openjfx;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+    public void txtUNameAction(ActionEvent actionEvent) {
+    }
+
+    public void txtUNameKeyPressed(KeyEvent keyEvent) {
+    }
+
+    public void cmbUIDAction(ActionEvent actionEvent) {
+    }
+
+    public void txtPWAction(ActionEvent actionEvent) {
+    }
+
+    public void txtPWKeyPressed(KeyEvent keyEvent) {
+    }
+
+    public void btnExitAction(ActionEvent actionEvent) {
+    }
+
+    public void ExitMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void ExitMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void btnLoginAction(ActionEvent actionEvent) {
+    }
+
+    public void LoginMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void LoginMouseExited(MouseEvent mouseEvent) {
+    }
+
+    public void lblForgotAction(ActionEvent actionEvent) {
+    }
+}
